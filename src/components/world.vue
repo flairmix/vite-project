43248376bfd34cfdac69f1bab4bfe4f94222a18f
Michail Donchenko <d9067246810@gmail.com>
2024-10-29 18:53:32 +0300

    add grid

--- src/components/world.vue
+++ src/components/world.vue
@@ -8,9 +8,11 @@ import * as OBC from "@thatopen/components";
 import { setupThreeScene } from "./utils/threeSetup";
 import { setupStats } from "./utils/statsSetup";
 
-// defineProps<{ msg: string }>()
 
 let container: HTMLElement;
+
+let backgroundColor = "#202932";
+
 
 onMounted(() => {
   const container = document.getElementById("container")!;
@@ -20,12 +22,57 @@ onMounted(() => {
     return;
   }
 
-  const { world, components } = setupThreeScene(container, 0x000000);
+  const { world, components } = setupThreeScene(container, backgroundColor);
+  
   setupStats(world);
 
   setupObjects(world);
   setupLighting(world);
   animate(world);
+
+  BUI.Manager.init();
+
+const panel = BUI.Component.create<BUI.PanelSection>(() => {
+return BUI.html`
+  <bim-panel label="Worlds Tutorial" class="options-menu">
+    <bim-panel-section collapsed label="Controls">
+
+      <bim-color-input 
+        label="Background Color" color=${backgroundColor} 
+        @input="${({ target }: { target: BUI.ColorInput }) => {
+          world.scene.config.backgroundColor = new THREE.Color(target.color);
+        }}">
+      </bim-color-input>
+
+      <bim-number-input 
+        slider step="0.1" label="Directional lights intensity" value="1.5" min="0.1" max="10"
+        @change="${({ target }: { target: BUI.NumberInput }) => {
+          world.scene.config.directionalLight.intensity = target.value;
+        }}">
+      </bim-number-input>
+
+      <bim-number-input 
+        slider step="0.1" label="Ambient light intensity" value="1" min="0.1" max="5"
+        @change="${({ target }: { target: BUI.NumberInput }) => {
+          world.scene.config.ambientLight.intensity = target.value;
+        }}">
+      </bim-number-input>
+
+    </bim-panel-section>
+  </bim-panel>
+  `;
+});
+
+document.body.append(panel);
+
+
+
+
+
+
+
+
+
 
 function setupObjects(world: any) {
   const material = new THREE.MeshLambertMaterial({ color: "#ff5522" });
@@ -54,15 +101,60 @@ function animate(world: any) {
 </script>
 
 <template>
-  <div id="container" style="width: 100vw; height: 95vh;"></div>
+  <div class="full-screen" id="container" style="width: 100vw; height: 95vh;">
+
+  </div>
 </template>
 
 <style scoped>
-  body, html {
-    margin: 0;
-    padding: 0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-  }
+ body {
+          margin: 0;
+          padding: 0;
+          font-family: "Plus Jakarta Sans", sans-serif;
+          overflow: hidden;
+      }
+
+      .full-screen {
+          width: 100vw;
+          height: 100vh;
+          position: relative;
+          overflow: hidden;
+      }
+
+      .options-menu {
+          position: fixed;
+          min-width: unset;
+          top: 5px;
+          right: 5px;
+          max-height: calc(100vh - 10px);
+      }
+
+      .phone-menu-toggler {
+          visibility: hidden;
+      }
+
+      @media (max-width: 480px) {
+          .options-menu {
+              visibility: hidden;
+              bottom: 5px;
+              left: 5px;
+          }
+
+          .options-menu-visible {
+              visibility: visible;
+          }
+
+          .phone-menu-toggler {
+              visibility: visible;
+              position: fixed;
+              top: 5px;
+              right: 5px;
+          }
+      }
+
+      .minimap {
+          position: fixed;
+          bottom: 1rem;
+          right: 1rem;
+      }
 </style>
